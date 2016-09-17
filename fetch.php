e6d51fb300ac325c7ccdb382cd80d637db8c88c4
Martin Rejda <rejdi@ksp.sk>
2016-09-17 23:24:31 +0200

    Added dummy values for first point. Fixes broken legend and make data more consistent.

--- fetch.php
+++ fetch.php
@@ -50,6 +50,11 @@ foreach ($json as $key => $item) {
 	}
 	if ($prev === null) {
 		$prev = $item;
+		$json[$key]['distance-total'] = 0.0;
+		$json[$key]['speed'] = 0.0;
+		$json[$key]['travel-time'] = 0;
+		$json[$key]['distance-air'] = 0.0;
+		$json[$key]['distance-diff'] = 0.0;
 		continue;
 	}
 	$json[$key]['distance-diff'] = getDistance($prev, $item);
